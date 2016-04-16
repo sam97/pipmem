cf1945927a70281cfe136b2bf1527bf5c32346b7
evitalis <evitalis@users.noreply.github.com>
2016-04-16 09:49:37 -0500

    Added (un)install subprocesses Uninstall subprocess run without prompting for confirmation Log package changes to pipmem.log

--- pipmem.py
+++ pipmem.py
@@ -1,7 +1,6 @@
-from subprocess import run
-
 import argparse
 import logging
+import subprocess
 
 # Create logger object and set appropriate format and filename
 pmlogger = logging.getLogger('pipmem')
@@ -14,11 +13,26 @@ pmlogger.addHandler(pmloghandler)
 
 
 def install_package(pkgs):
-    print('Installing...%s' % pkgs)
-
+    for pkg in pkgs.split(','):
+        output = subprocess.run(['pip', 'install', pkg],
+                                stdout=subprocess.PIPE,
+                                universal_newlines=True)
+        print(output.stdout)
+        for line in output.stdout.split('\n'):
+            if 'Successfully' in line:
+                for ipkg in line.split(' ')[2:]:
+                    pmlogger.info('Installed %s', ipkg)
 
 def uninstall_package(pkgs):
-    print('Uninstalling...%s' % pkgs)
+    for pkg in pkgs.split(','):
+        output = subprocess.run(['pip', 'uninstall', '-y', pkg],
+                                stdout=subprocess.PIPE,
+                                universal_newlines=True)
+        print(output.stdout)
+        for line in output.stdout.split('\n'):
+            if 'Successfully' in line:
+                for ipkg in line.split(' ')[4:]:
+                    pmlogger.info('Uninstalled %s', ipkg)
 
 
 if __name__ == '__main__':
@@ -32,10 +46,10 @@ if __name__ == '__main__':
     action_uninstall = actions.add_parser('uninstall',
                                           help='Unnstall packages.')
     ipkgs = action_install.add_argument('-p', '--pkgs',
-                                        action='append',
+                                        action='store',
                                         help='List of packages to install')
     upkgs = action_uninstall.add_argument('-p', '--pkgs',
-                                          action='append',
+                                          action='store',
                                           help='List of packages to install')
     args = parser.parse_args()
 
