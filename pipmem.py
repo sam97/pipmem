2df5f0fa377e721b8d6b8fb186937f9b5dd87da0
evitalis <evitalis@users.noreply.github.com>
2016-04-16 11:45:39 -0500

    Store transaction data in sqlite3 database

--- pipmem.py
+++ pipmem.py
@@ -1,5 +1,8 @@
 import argparse
+import datetime
 import logging
+import os
+import sqlite3
 import subprocess
 
 # Create logger object and set appropriate format and filename
@@ -11,28 +14,55 @@ pmloghandler = logging.FileHandler('pipmem.log')
 pmloghandler.setFormatter(pmlogformat)
 pmlogger.addHandler(pmloghandler)
 
+pmdbfile = 'pipmem.db'
+
+
+def setupdb():
+    conn = sqlite3.connect(pmdbfile)
+    conn.execute('CREATE TABLE transactions ( \
+                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
+                 timestamp TEXT NOT NULL, \
+                 action TEXT NOT NULL, \
+                 pkgs TEXT NOT NULL, \
+                 venv TEXT)')
+
+
+def insert_transaction(action, pkgs):
+    now = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    pkgs = ','.join(pkgs)
+
+    conn = sqlite3.connect(pmdbfile)
+    conn.execute('INSERT INTO transactions (timestamp, action, pkgs, venv) \
+                 VALUES (?, ?, ?, ?)', (now, action, pkgs, None))
+    conn.commit()
+    conn.close()
+
 
 def install_package(pkgs):
-    for pkg in pkgs.split(','):
-        output = subprocess.run(['pip', 'install', pkg],
-                                stdout=subprocess.PIPE,
-                                universal_newlines=True)
-        print(output.stdout)
-        for line in output.stdout.split('\n'):
-            if 'Successfully' in line:
-                for ipkg in line.split(' ')[2:]:
-                    pmlogger.info('Installed %s', ipkg)
+    output = subprocess.run(['pip', 'install'] + pkgs.split(','),
+                            stdout=subprocess.PIPE,
+                            universal_newlines=True)
+    print(output.stdout)
+    for line in output.stdout.split('\n'):
+        if 'Successfully installed' in line:
+            ipkgs = line.split(' ')[2:]
+            insert_transaction('install', ipkgs)
+            for ipkg in ipkgs:
+                pmlogger.info('Installed %s', ipkg)
+
 
 def uninstall_package(pkgs):
-    for pkg in pkgs.split(','):
-        output = subprocess.run(['pip', 'uninstall', '-y', pkg],
-                                stdout=subprocess.PIPE,
-                                universal_newlines=True)
-        print(output.stdout)
-        for line in output.stdout.split('\n'):
-            if 'Successfully' in line:
-                for ipkg in line.split(' ')[4:]:
-                    pmlogger.info('Uninstalled %s', ipkg)
+    output = subprocess.run(['pip', 'uninstall', '-y'] + pkgs.split(','),
+                            stdout=subprocess.PIPE,
+                            universal_newlines=True)
+    print(output.stdout)
+    upkgs = []
+    for line in output.stdout.split('\n'):
+        if 'Successfully uninstalled' in line:
+            upkgs += line.split(' ')[4:]
+            insert_transaction('uninstall', upkgs)
+            for upkg in upkgs:
+                pmlogger.info('Uninstalled %s', upkg)
 
 
 if __name__ == '__main__':
@@ -52,6 +82,9 @@ if __name__ == '__main__':
                                           action='store',
                                           help='List of packages to install')
     args = parser.parse_args()
+
+    if not os.path.exists(pmdbfile):
+        setupdb()
 
     if args.action == 'install':
         install_package(args.pkgs)
